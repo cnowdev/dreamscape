ea508ab679b18dbf50803621dad92cd9f6a6f14c
cnow <siddhu.vsss@gmail.com>
2023-07-16 14:44:33 -0400

    added font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Quicksand_400Regular  } from '@expo-google-fonts/quicksand';
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Quicksand_400Regular,
+  })
+
+  if(!fontsLoaded) { return null; }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{fontFamily: 'Quicksand_400Regular', fontSize: 30}}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
