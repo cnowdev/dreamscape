1e2ff41cb4536b8f9c5efcc30061f7acb6eeefd2
cnow <siddhu.vsss@gmail.com>
2023-07-16 17:24:22 -0400

    header updates

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useFonts, Quicksand_400Regular  } from '@expo-google-fonts/quicksand';
+import { useFonts, Quicksand_400Regular, Quicksand_700Bold  } from '@expo-google-fonts/quicksand';
 import { Header } from '@rneui/themed'
 import { Ionicons } from '@expo/vector-icons'; 
 import { Feather } from '@expo/vector-icons'; 
@@ -10,6 +10,7 @@ import { Feather } from '@expo/vector-icons';
 export default function App() {
   let [fontsLoaded] = useFonts({
     Quicksand_400Regular,
+    Quicksand_700Bold
   })
 
   if(!fontsLoaded) { return null; }
@@ -17,15 +18,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Header
-        backgroundColor='#A491D3'
-        centerComponent={{text: 'BLAH', style: {color: '#fff', fontFamily: 'Quicksand_400Regular', fontSize: 30, marginTop: 20}}}
-        leftComponent={<Ionicons name="md-reorder-three-outline" size={24} color="white" style={{marginTop: 20, paddingLeft: 20}} />}
-        rightComponent={<Feather name="search" size={24} color="white" style={{marginTop: 20, paddingRight: 20}} />}
-        elevated={true}
-        containerStyle={{height: 100, justifyContent: 'center'}}
+        backgroundColor='#040F16'
+        centerComponent={{text: 'Dreamscape', style: {color: '#fff', fontFamily: 'Quicksand_700Bold', fontSize: 30, marginTop: 25, fontWeight: 'bold'}}}
+        leftComponent={<Ionicons name="md-reorder-three-outline" size={24} color="white" style={{marginTop: 30, paddingLeft: 20}} />}
+        rightComponent={<Feather name="search" size={24} color="white" style={{marginTop: 30, paddingRight: 20}} />}
+        containerStyle={{height: 100, justifyContent: 'center', borderBottomWidth: 0}}
+        
       />
       <Text style={{fontFamily: 'Quicksand_400Regular', fontSize: 30, marginTop: 20, color: '#fff'}}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" translucent />
+      <StatusBar style="light" translucent />
     </View>
   );
 }
@@ -33,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#240115',
+    backgroundColor: '#040F16',
     alignItems: 'center',
 
   },
