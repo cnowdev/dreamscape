b563b6bd523824fe16be6257550857d21c158649
rohcoding46 <ragarwal.edu@gmail.com>
2023-07-17 21:55:59 -0400

    start of dream editor

--- DreamEditor.tsx
+++ DreamEditor.tsx
@@ -1,28 +1,27 @@
-import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { RootStackParamList } from './types';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-
+import Form from './components/Form'; 
 
 type navigationProps = NativeStackScreenProps<RootStackParamList, 'DreamEditor'>;
 
 interface Props {
-    id?: string,
-    navigation: navigationProps['navigation']
+  id?: string;
+  navigation: navigationProps['navigation'];
 }
 
-export default function DreamEditor({navigation, id}: Props ) {
+export default function DreamEditor({ navigation, id }: Props) {
   return (
     <View style={styles.container}>
-        <Text style={{color: '#fff'}}>Test</Text>
-        <TextInput></TextInput>
+      <Form title = "Title" description="Description"></Form>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#040F16',
-
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#040F16',
+  },
 });
---
+++ components/Form.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleTitleChange = (text) => {
+    setTitle(text);
+  };
+
+  const handleDescriptionChange = (text) => {
+    setDescription(text);
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.titleInput}
+        value={title}
+        onChangeText={handleTitleChange}
+        placeholder="Enter a title"
+        placeholderTextColor="#fff"
+      />
+
+      <TextInput
+        style={styles.descriptionInput}
+        value={description}
+        onChangeText={handleDescriptionChange}
+        placeholder="Enter a description"
+        placeholderTextColor="#fff"
+        multiline={true} // Allow multiple lines for the description
+        numberOfLines={4} // Control the height by adjusting the number of lines displayed
+      />
+    </View>
+  );
+};
+
+const styles = {
+  titleInput: {
+    fontSize: 18,
+    color: '#fff',
+    backgroundColor: '#092333',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    marginLeft: 20,
+    fontFamily: 'Quicksand_400Regular', 
+    width: 350
+  },
+  descriptionInput: {
+    fontSize: 18,
+    color: '#fff',
+    backgroundColor: '#092333',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    marginLeft: 20, 
+    height: 200,
+    fontFamily: 'Quicksand_400Regular',
+    width: 350 
+  },
+};
+
+export default Form;
