07412d86755d76d35607b0b1009be7ce618a95ea
rohcoding46 <ragarwal.edu@gmail.com>
2023-07-18 22:23:06 -0400

    key-value pair

--- DreamEditor.tsx
+++ DreamEditor.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { RootStackParamList } from './types';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RouteProp } from '@react-navigation/native';
 import { useState } from 'react';
+import * as SecureStore from 'expo-secure-store'
+import 'react-native-get-random-values'
+{/*
+          @ts-ignore */}
+import { v4 as uuidv4 } from 'uuid';
+
+
 type navigationProps = NativeStackScreenProps<RootStackParamList, 'DreamEditor'>;
-
-const img = require('./assets/lmao.jpg')
-
 
 interface Props {
   navigation: navigationProps['navigation'],
@@ -26,48 +30,42 @@ export default function DreamEditor({ navigation, route }: Props) {
 
   return (
     <View style={styles.container}>
-    <Image source={img} style={styles.image}/>
-
-    <View style={styles.textcontainer}>
       <View>
         <TextInput
-        style={styles.titleInput}
-        value={title}
-        onChangeText={(e: string) => {setTitle(e)}}
-        placeholder="Enter a title"
-        placeholderTextColor="#D3D3D3"
+          style={styles.titleInput}
+          value={title}
+          onChangeText={(e: string) => { setTitle(e) }}
+          placeholder="Enter a title"
+          placeholderTextColor="#D3D3D3"
         />
 
         <TextInput
-        style={styles.descriptionInput}
-        value={description}
-        onChangeText={(e: string) => {setDescription(e)}}
-        placeholder="Enter a description"
-        placeholderTextColor="#D3D3D3"
-        multiline={true} // Allow multiple lines for the description
-        numberOfLines={4} // Control the height by adjusting the number of lines displayed
+          style={styles.descriptionInput}
+          value={description}
+          onChangeText={(e: string) => { setDescription(e) }}
+          placeholder="Enter a description"
+          placeholderTextColor="#D3D3D3"
+          multiline={true}
+          numberOfLines={4} 
         />
-        <Text style={styles.AIdescription}>
-          AI Continuation: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-        </Text>
-      </View>
-    </View>
-    <TouchableOpacity style={styles.button}>
+        
+        <TouchableOpacity style={styles.button} onPress={async () => {
+          const key = uuidv4();
+          const value = {title: title, description: description};
+            await SecureStore.setItemAsync(key, JSON.stringify(value));
+            console.log(key)
+            console.log(value)
+        }}>
           <Text style={styles.buttonText}>Save</Text>
         </TouchableOpacity>
+
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: 400,
-    height: 200,
-    marginHorizontal: 10
-  },
-  textcontainer: {
-    marginTop: 20,
-  },
   container: {
     flex: 1,
     backgroundColor: '#040F16',
@@ -90,21 +88,11 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginBottom: 20,
-    marginLeft: 20, 
-    height: 150,
+    marginLeft: 20,
+    height: 200,
     fontFamily: 'Quicksand_400Regular',
     width: 350,
     textAlignVertical: 'top'
-  },
-  AIdescription: {
-    fontSize: 16,
-    color: '#fff',
-    backgroundColor: '#092333',
-    borderRadius: 5,
-    padding: 10,
-    marginLeft: 20,
-    fontFamily: 'Quicksand_400Regular',
-    width: 350,
   },
   button: {
     backgroundColor: '#52aae0',
